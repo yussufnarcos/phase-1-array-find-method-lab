cb8a36dc1e75f424d95db04a49d4ceebf1734d60
yussufnarcos <narcosyussuf@gmail.com>
2025-03-13 16:59:59 +0300

    phase-1-array-find-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code your solution here
+function superbowlWin(record) {
+    let win = record.find(game => game.result === "W");
+    return win ? win.year : undefined;
+}
+
+console.log(superbowlWin(broncosRecord)); 
